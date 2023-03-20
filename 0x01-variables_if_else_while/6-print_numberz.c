3256417c409448fdab420637f4444e5e583e0a20
aaabdelali980 <aliabdel726@gmail.com>
2023-03-20 09:40:39 -0700

    print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry print single digit numbers to base 10
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+
+for (i = 0; i <= 9 ; i++)
+{
+putchar(i + '0');
+}
+putchar('\n)
+return (0);
+}
