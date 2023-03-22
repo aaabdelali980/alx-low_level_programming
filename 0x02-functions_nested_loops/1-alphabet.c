03c4a2635e056a6be51141ad1eff4438e0a4f994
aaabdelali980 <aliabdel726@gmail.com>
2023-03-22 11:39:44 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,12 @@
 #include "main.h"
-/**
- * print_alphabet - function that prints alphabet in lower case
- *
- * Return: Always 0
- */
-void print_alphabet(void);
-{
-	int i;
 
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-	_putchar('\n');
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
 }
