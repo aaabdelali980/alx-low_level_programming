d26eef71563a3bb68b49310834bcc634fabdd6e0
aaabdelali980 <aliabdel726@gmail.com>
2023-03-22 09:25:06 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point main function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+}
