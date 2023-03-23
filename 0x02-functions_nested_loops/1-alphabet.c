3ab9d76222110fa0a99ba56f9a296c2387e899b0
aaabdelali980 <aliabdel726@gmail.com>
2023-03-22 19:17:32 -0700

    alphabe

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,4 +10,5 @@ void print_alphapet(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for Holberton School students.
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 int main(void)
 {
